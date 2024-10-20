d7c7616f7c1e3879b9c921a693df2afeddcbd564
luizpauloduarte <luizduarte1989@gmail.com>
2024-10-20 20:10:17 +0200

    Trabalho EcoEduca

---
+++ mf-des-backend-2024/Models/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_des_backend_2024.Models
+{
+
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
+
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        [Display(Name = "Ano do Modelo")]
+
+        public int AnoModelo { get; set; }
+    }
+}
